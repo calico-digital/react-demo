f37c78a8c535eaef5b00a6490929961d9e867571
Josh Griggs <josh@calico.digital>
2023-04-19 07:56:44 +0100

    Pokemon list

--- components/PokemonList.jsx
+++ components/PokemonList.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function PokemonListItem({pokemon}) {
-    return(
-        <li className="border border-gray-400 p-5 flex items-center space-x-4">
-            {/* dot */}
-            <span className="text-gray-700 text-xl">{pokemon.name}</span>
-        </li>
-        )
+function PokemonListItem({ pokemon, test }) {
+  return (
+    <li className={`border border-gray-400 p-5 flex items-center space-x-4 ${getColour(pokemon.type1)}`}>
+      {/* dot */}
+      <span className="text-gray-700 text-xl">{pokemon.name} {test}</span>
+    </li>
+  );
 }
 
+function getColour(pokemonType) {
+    switch (pokemonType) {
+        case 'grass':
+            return 'bg-green-600';
+        case 'poison':
+            return 'bg-yellow-600';
+        case 'water':
+            return 'bg-blue-600';
+        case 'bug':
+            return 'bg-red-600';
+        default:
+            return 'bg-gray-600';
+    }
+}
 
-export default function PokemonList({pokemons}) {
+export default function PokemonList({ pokemons }) {
   return (
     <ul>
-        {
-            pokemons.length > 0 ? 
-                pokemons.map((pokemon, index)=> <PokemonListItem pokemon={pokemon} key={index} />)
-            : <h1 className="text-3xl text-gray-600"> No pokemons in database </h1> 
-        }
+      {pokemons.length > 0 ? (
+        pokemons.map((individualPokemon, index) => (
+          <PokemonListItem pokemon={individualPokemon} test={index} key={index} />
+        ))
+      ) : (
+        <h1 className="text-3xl text-gray-600"> No pokemons in database </h1>
+      )}
     </ul>
-  )
+  );
 }
